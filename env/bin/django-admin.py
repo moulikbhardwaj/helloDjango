16c3c485452ccf783a8641013d126e29ec31787f
Moulik <moulikbhardwaj@gmail.com>
2019-10-09 22:21:22 +0530

    commit

--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/moulik/github/helloDjango/env/bin/python3
+#!/home/moulik/github/helloDjango/env/bin/python
 from django.core import management
 
 if __name__ == "__main__":
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 import json
-from .models import Question
+from .models import Question, Choice
+from django.urls import reverse
 
 def index(request):
 	latest_question_list = Question.objects.order_by('pub_date')[:5]
@@ -19,4 +20,12 @@ def results(request, question_id):
 	return HttpResponse('You are looking at results of question {}'.format(question_id))
 
 def vote(request, question_id):
-	return HttpResponse('You are voting on question {}'.format(question_id))
+	question = get_object_or_404(Question,pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except(KeyError,Choice.DoesNotExist):
+		return render(request, 'polls/detail.html',{'question':question, 'error_message':'You did\'nt selected a choice'})
+	else:
+		selected_choic.vote+=1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
